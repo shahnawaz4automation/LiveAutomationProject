61b170256f1b29af810220830e0765a9ff083816
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-28 12:53:40 +0530

    first commit

---
+++ src/main/java/lap/LiveAutomationProject/App.java
@@ -0,0 +1,13 @@
+package lap.LiveAutomationProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
