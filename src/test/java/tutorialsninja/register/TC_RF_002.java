8d01e09e261a6e095c80c180d5736b45fe352689
Mohammed Shahnawaz <shah4corp@gmail.com>
2024-12-30 20:45:34 +0530

    30122045 commit

--- src/test/java/tutorialsninja/register/TC_RF_002.java
+++ src/test/java/tutorialsninja/register/TC_RF_002.java
@@ -1,10 +1,12 @@
 package tutorialsninja.register;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TC_RF_002 {
 	@Test
 	public void validateEmail() {
-		
+		WebDriver driver = new ChromeDriver();
 	}
 }
