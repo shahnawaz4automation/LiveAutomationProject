c0c1ab735aaa23418971b765e55ff62ed026b7dd
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-01-01 11:52:21 +0530

    01011152 commit

--- src/test/java/tutorialsninja/register/TC_RF_002.java
+++ src/test/java/tutorialsninja/register/TC_RF_002.java
@@ -16,6 +16,13 @@ public class TC_RF_002 {
 		driver.get("https://www.amazon.in/");
 		
 		driver.findElement(By.xpath("//span[text()='Hello, sign in']")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Need help?')]")).click();
+		driver.findElement(By.id("auth-fpp-link-bottom")).click();
 		
+		String email = "shahnawaz4automation@gmail.com";
+		String appPasscode = "cpmz aupa mjbj ijsf";
+		
+		driver.findElement(By.id("ap_email")).sendKeys(email);
+		driver.findElement(By.id("continue")).click();
 	}
 }
