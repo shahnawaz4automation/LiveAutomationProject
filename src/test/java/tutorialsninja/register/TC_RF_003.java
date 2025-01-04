b477ce9b5aec603e61e50cf0e0c0e358bc000d37
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-01-04 16:26:52 +0530

    04011626 commit

--- src/test/java/tutorialsninja/register/TC_RF_003.java
+++ src/test/java/tutorialsninja/register/TC_RF_003.java
@@ -1,8 +1,8 @@
 package tutorialsninja.register;
 
-import static org.testng.Assert.assertTrue;
 import java.time.Duration;
 import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
