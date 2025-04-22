731883f1b7b5a6250c64202be909d307b765b2e4
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-04-22 22:19:48 +0530

    98u23jrfq commit

--- src/test/java/tutorialsninja/register/TC_RF_001.java
+++ src/test/java/tutorialsninja/register/TC_RF_001.java
@@ -3,8 +3,7 @@ package tutorialsninja.register;
 public class TC_RF_001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Will start working on this from tomorrow");
 	}
 
 }
