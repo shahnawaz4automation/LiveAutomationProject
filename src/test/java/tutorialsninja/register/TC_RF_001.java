0321f3dc82bdd723ad4a808dc3a1726d2c1497ad
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-04-22 22:20:41 +0530

    rqfwasc commit

--- src/test/java/tutorialsninja/register/TC_RF_001.java
+++ src/test/java/tutorialsninja/register/TC_RF_001.java
@@ -5,6 +5,7 @@ public class TC_RF_001 {
 	public static void main(String[] args) {
 		System.out.println("Will start working on this from tomorrow");
 		System.out.println("Inshallah");
+		System.out.println("This program need to be re-written");
 	}
 
 }
