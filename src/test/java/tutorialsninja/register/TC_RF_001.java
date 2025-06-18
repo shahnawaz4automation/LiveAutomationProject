cfa529363497cfc01c40ad26651129616eac304e
Mohammed Shahnawaz <shah4corp@gmail.com>
2025-06-18 08:59:16 +0530

    fasv commit

--- src/test/java/tutorialsninja/register/TC_RF_001.java
+++ src/test/java/tutorialsninja/register/TC_RF_001.java
@@ -4,7 +4,7 @@ public class TC_RF_001 {
 
 	public static void main(String[] args) {
 		System.out.println("Will start working on this from tomorrow");
-		System.out.println("Inshallah");
+		//System.out.println("Inshallah");
 		
 	}
 
